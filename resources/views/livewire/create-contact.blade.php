82fb7cebe30ce410d360d601cc6da3a3b954b574
Ivan <ivanwoogue@gmail.com>
2024-07-05 13:23:00 +0800

    okay

---
+++ resources/views/livewire/create-contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="alert alert-danger">Hello</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\CreateContact;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/create", CreateContact::class);
